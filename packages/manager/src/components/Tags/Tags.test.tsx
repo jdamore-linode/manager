91baa4be8682cfc51ece56f8448b5d2952b318d4
Joe D'Amore <jdamore@linode.com>
2022-02-11 14:03:55 -0500

    Replace enzyme with React Testing Library for class component

--- packages/manager/src/components/Tags/Tags.test.tsx
+++ packages/manager/src/components/Tags/Tags.test.tsx
@@ -1,34 +1,51 @@
-import { shallow } from 'enzyme';
 import * as React from 'react';
-
+import { renderWithTheme } from 'src/utilities/testHelpers';
 import { Tags } from './Tags';
 
+const threeTags = ['Tag 1', 'Tag 2', 'Tag 3'];
+
+const fiveTags = ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5'];
+
+const classes = {
+  root: '',
+  tag: '',
+};
+
 describe('Tags list', () => {
-  it('should display "Show More" button if the tags list is more than 3', () => {
-    const component = shallow(
-      <Tags
-        tags={['tag1', 'tag2', 'tag3', 'tag4', 'tag5']}
-        classes={{
-          root: '',
-          tag: '',
-        }}
-      />
+  it('Should display only first 3 tags when more than 3 tags are in tags list', () => {
+    const { getByText, queryByText } = renderWithTheme(
+      <Tags tags={fiveTags} classes={classes} />
     );
 
-    expect(component.find('WithStyles(ShowMore)')).toHaveLength(1);
+    // Check that first 3 tags are displayed.
+    fiveTags.slice(0, 3).forEach((tag) => {
+      expect(getByText(tag)).toBeVisible();
+    });
+
+    // Check that remaining tags are hidden.
+    fiveTags.slice(3).forEach((tag) => {
+      expect(queryByText(tag)).toBeNull();
+    });
   });
 
-  it('shouldn\'t display the "Show More" button if the tags list contains 3 or fewer tags', () => {
-    const component = shallow(
-      <Tags
-        tags={['tag1', 'tag2', 'tag3']}
-        classes={{
-          root: '',
-          tag: '',
-        }}
-      />
+  it('Should have a show more button when more than 3 tags are in tags list', () => {
+    const { container, getByLabelText } = renderWithTheme(
+      <Tags tags={fiveTags} classes={classes} />
     );
 
-    expect(component.find('WithStyles(ShowMore)')).toHaveLength(0);
+    expect(getByLabelText('+2 tags')).toBeVisible();
+    expect(
+      container.querySelector('[data-qa-show-more-chip="true"]')
+    ).toBeVisible();
+  });
+
+  it('Should not have show more button when 3 or fewer tags are in tags list', () => {
+    const { container } = renderWithTheme(
+      <Tags tags={threeTags} classes={classes} />
+    );
+
+    expect(
+      container.querySelector('[data-qa-show-more-chip="true"]')
+    ).toBeNull();
   });
 });
