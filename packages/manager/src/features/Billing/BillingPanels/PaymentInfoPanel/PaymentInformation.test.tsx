d05c6128a700f93f31df601c9b28c504567718ee
Joe D'Amore <jdamore@linode.com>
2022-03-16 11:20:28 -0400

    Uncomment different test

--- packages/manager/src/features/Billing/BillingPanels/PaymentInfoPanel/PaymentInformation.test.tsx
+++ packages/manager/src/features/Billing/BillingPanels/PaymentInfoPanel/PaymentInformation.test.tsx
@@ -1,4 +1,4 @@
-//import { fireEvent } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import PaymentInformation from './PaymentInformation';
 import { paymentMethodFactory } from 'src/factories';
 import { renderWithTheme } from 'src/utilities/testHelpers';
@@ -30,28 +30,28 @@ describe('Payment Info Panel', () => {
   //   expect(getByLabelText('Content is loading')).toBeVisible();
   // });
 
-  // it('Opens "Add Payment Method" drawer when "Add Payment Method" is clicked', () => {
-  //   const { getByTestId } = renderWithTheme(
-  //     <PaymentInformation loading={false} paymentMethods={paymentMethods} />
-  //   );
-
-  //   const addPaymentMethodButton = getByTestId(
-  //     'payment-info-add-payment-method'
-  //   );
-
-  //   fireEvent.click(addPaymentMethodButton);
-  //   expect(getByTestId('drawer')).toBeVisible();
-  // });
-
-  it('Lists all payment methods', () => {
+  it('Opens "Add Payment Method" drawer when "Add Payment Method" is clicked', () => {
     const { getByTestId } = renderWithTheme(
       <PaymentInformation loading={false} paymentMethods={paymentMethods} />
     );
 
-    paymentMethods.forEach((paymentMethod) => {
-      expect(
-        getByTestId(`payment-method-row-${paymentMethod.id}`)
-      ).toBeVisible();
-    });
+    const addPaymentMethodButton = getByTestId(
+      'payment-info-add-payment-method'
+    );
+
+    fireEvent.click(addPaymentMethodButton);
+    expect(getByTestId('drawer')).toBeVisible();
   });
+
+  // it('Lists all payment methods', () => {
+  //   const { getByTestId } = renderWithTheme(
+  //     <PaymentInformation loading={false} paymentMethods={paymentMethods} />
+  //   );
+
+  //   paymentMethods.forEach((paymentMethod) => {
+  //     expect(
+  //       getByTestId(`payment-method-row-${paymentMethod.id}`)
+  //     ).toBeVisible();
+  //   });
+  // });
 });
